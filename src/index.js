7212984b6423ba14ebf3ebf0634b33233fad2f5e
chebupell <kremenvov11@gmail.com>
2025-09-07 21:25:05 +0300

    feat: end

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const newNum = Math.abs(n).toString();
+  const reversedStr = newNum.split('').reverse().join('');
+  return reversedStr;
 };
